2c95c8923689d3a08d2edccf6f56564e394544df
Griffin Ledingham <gcledingham@gmail.com>
2023-12-02 15:21:29 -0800

    02_02 comments

--- src/problems/02_02/02_02.ts
+++ src/problems/02_02/02_02.ts
@@ -3,12 +3,17 @@ import { Colors } from "./fixtures/input";
 
 export const solve02_02 = (input: string) => {
   time("solve02_02");
+  // Sum all games' power
   const power = input.split("\n").reduce((acc, line) => {
+    // Iterate through each game and find the multiplicitive power of the game
     const powerForGame = Object.values(Colors).reduce((acc, color) => {
       return (
         acc *
+        // Max all occurences of the color in the line
         Math.max(
+          // Math all occurences of the color in the line
           ...(line?.match(new RegExp(`(\\d+) ${color}`, "g")) ?? [])?.map((i) =>
+            // Parse the number from the match
             parseInt(i.replace(` ${color}`, ""))
           )
         )
